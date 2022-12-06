29d950c8905a40ef4d69003f21b067ac81eed744
Jakob <jakob.lindqvist@chalmers.se>
2022-12-06 09:40:01 +0100

    Indices, some tests failing

--- src/nce/per_cnce.py
+++ src/nce/per_cnce.py
@@ -20,31 +20,39 @@ class PersistentCondNceCrit(PartFnEstimator):
 
     def __init__(self, unnorm_distr, noise_distr, num_neg_samples):
         super().__init__(unnorm_distr, noise_distr, num_neg_samples)
-        self._persistent_y = None
+        self._persistent_y = dict()
 
-    def crit(self, y: Optional[Tensor]) -> Tensor:
-        print("y:", y.shape)
-        y_p = self.persistent_y(y)
+    def crit(self, y: Tensor, idx: Optional[Tensor]) -> Tensor:
+        assert (
+            idx is not None
+        ), "PersistentCondNceCrit requires an idx tensor that is not None"
+        y_p = self.persistent_y(y, idx)
         y_samples = self.sample_noise(self._num_neg * y_p.size(0), y_p)
         # NB We recompute w_tilde in inner_crit to comply with the API.
         w_tilde = self._unnorm_w(y, y_samples)
-        self._update_persistent_y(w_tilde, y_p, y_samples)
+        self._update_persistent_y(w_tilde, y_samples, y_p, idx)
         return self.inner_crit(y, y_samples)
 
     def inner_crit(self, y: Tensor, y_samples: Tensor):
         w_tilde = self._unnorm_w(y, y_samples)
         return torch.log(1 + (1 / w_tilde)).mean()
 
-    def persistent_y(self, actual_y: Tensor):
+    def persistent_y(self, actual_y: Tensor, idx: Tensor):
         """Get persistent y
 
         Access the last selected y or return the y sampled from p_d
         if no last selected y exists
         """
-        per_y = self._persistent_y if self._persistent_y is not None else actual_y
+        per_y = torch.empty(actual_y.size())
+        for i, per_i in enumerate(idx):
+            per_y[i] = (
+                self._persistent_y[per_i]
+                if self._persistent_y[per_i] is not None
+                else actual_y[i]
+            )
         return per_y
 
-    def _update_persistent_y(self, w_unnorm, y, y_samples):
+    def _update_persistent_y(self, w_unnorm, y_samples, y, idx):
         """Sample new persistent y"""
         ys = extend_sample(y, y_samples)
         idx = Categorical(w_unnorm).sample()
--- tests/nce/test_pers_cnce.py
+++ tests/nce/test_pers_cnce.py
@@ -1,6 +1,5 @@
 import unittest
 import torch
-import numpy as np
 
 from src.noise_distr.conditional_normal import ConditionalMultivariateNormal
 from src.nce.per_cnce import PersistentCondNceCrit
@@ -14,19 +13,29 @@ class TestPersistentCnce(unittest.TestCase):
         crit = PersistentCondNceCrit(None, None, J)
         w_unnorm = torch.ones((J + 1,))
         y_samples = torch.ones((J + 1,))
-        y = torch.ones((1,))
-        crit._update_persistent_y(w_unnorm, y, y_samples)
-        y_p = crit.persistent_y(torch.randn(y.size()))
+        y, idx = torch.ones((1,)), torch.zeros((1,))
+        crit._update_persistent_y(w_unnorm, y, y_samples, idx)
+        y_p = crit.persistent_y(torch.randn(y.size()), idx)
         self.assertAlmostEqual(y_p.item(), 1)
 
-    def test_y_persistent_update(self):
+    def test_y_persistent_dim(self):
+        J, D = 5, 10
+        crit = PersistentCondNceCrit(None, None, J)
+        w_unnorm = torch.ones((J + 1,))
+        y_samples = torch.ones((J + 1, D))
+        y, idx = torch.ones((1,)), torch.zeros((1,))
+        crit._update_persistent_y(w_unnorm, y, y_samples, idx)
+        y_p = crit.persistent_y(torch.randn(y.size()), idx)
+        self.assertEqual(y_p.size(), y.size())
+
+    def test_y_persistent_multi_dim(self):
         J = 5
         crit = PersistentCondNceCrit(None, None, J)
         w_unnorm = torch.ones((J + 1,))
         y_samples = torch.ones((J + 1,))
-        y = torch.ones((1,))
-        crit._update_persistent_y(w_unnorm, y, y_samples)
-        y_p = crit.persistent_y(torch.randn(y.size()))
+        y, idx = torch.ones((1,)), torch.zeros((1,))
+        crit._update_persistent_y(w_unnorm, y, y_samples, idx)
+        y_p = crit.persistent_y(torch.randn(y.size()), idx)
         self.assertAlmostEqual(y_p.item(), 1)
 
 
