a6fc003b21f416682a782da5d1ce59c6a4ea4461
Jakob <jakob.lindqvist@chalmers.se>
2022-12-21 16:18:02 +0100

    KL div comparison exp

--- src/training/model_training.py
+++ src/training/model_training.py
@@ -6,48 +6,6 @@ from src.training.training_utils import no_stopping
 from src.noise_distr.normal import MultivariateNormal
 
 
-def train_model_model_proposal(
-    model,
-    crit_constructor,
-    evaluation_metric,
-    train_loader,
-    save_dir,
-    neg_sample_size: int = 10,
-    num_epochs: int = 100,
-    stopping_condition=no_stopping,
-    lr: float = 0.1,
-):
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-
-    metric = []
-    for epoch in range(1, num_epochs + 1):
-        # print(f"Epoch {epoch}")
-        running_loss = 0.0
-        old_params = torch.nn.utils.parameters_to_vector(model.parameters())
-        for _, (y, idx) in enumerate(train_loader, 0):
-            q = MultivariateNormal(
-                model.mu.detach().clone(), model.cov().clone().detach().clone()
-            )
-            criterion = crit_constructor(model, q, neg_sample_size)
-            optimizer.zero_grad()
-            with torch.no_grad():
-                loss = criterion.crit(y, None)
-                # print(loss)
-            # Calculate and assign gradients
-            criterion.calculate_crit_grad(y, idx)
-
-            # Take gradient step
-            optimizer.step()
-
-            # running_loss += loss.item()
-            metric.append(evaluation_metric(model).detach().numpy())
-        if stopping_condition(
-            torch.nn.utils.parameters_to_vector(model.parameters()), old_params
-        ):
-            print("Training converged")
-            break
-
-
 def train_model(
     criterion,
     evaluation_metric,
@@ -62,7 +20,9 @@ def train_model(
     model = criterion.get_model()
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
-    metric = []
+    batch_metrics = []
+    batch_metrics.append(evaluation_metric(model))
+    batch_losses = []
     for epoch in range(num_epochs):
 
         old_params = torch.nn.utils.parameters_to_vector(model.parameters())
@@ -77,7 +37,10 @@ def train_model(
             optimizer.step()
 
             # Note: now logging this for every iteration (and not epoch)
-            metric.append(evaluation_metric(model).detach().numpy())
+            with torch.no_grad():
+                loss = criterion.crit(y, None)
+                batch_losses.append(loss.item())
+            batch_metrics.append(evaluation_metric(model))
 
         # print('[%d] evaluation metric: %.3f' %
         #       (epoch + 1, metric[-1]))
@@ -90,9 +53,58 @@ def train_model(
 
     print("Finished training")
 
-    metric = np.array(metric)
     if save_dir is not None:
-        np.save(save_dir, metric)
+        np.save(save_dir, batch_metrics)
         print("Data saved")
 
-    return metric[-1]
+    return torch.tensor(batch_losses), torch.tensor(batch_metrics)
+
+
+def train_model_model_proposal(
+    model,
+    crit_constructor,
+    evaluation_metric,
+    train_loader,
+    save_dir,
+    neg_sample_size: int = 10,
+    num_epochs: int = 100,
+    stopping_condition=no_stopping,
+    lr: float = 0.1,
+):
+    """Training loop for q = p_theta
+
+    Training loop for idealised experiment where we have access to normalised p_theta
+    such that we can sample from it and use q = p_theta as the proposal distribution.
+    """
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+
+    batch_metrics = []
+    batch_metrics.append(evaluation_metric(model))
+    batch_losses = []
+    for epoch in range(1, num_epochs + 1):
+        # print(f"Epoch {epoch}")
+        old_params = torch.nn.utils.parameters_to_vector(model.parameters())
+        for _, (y, idx) in enumerate(train_loader, 0):
+            q = MultivariateNormal(
+                model.mu.detach().clone(), model.cov().clone().detach().clone()
+            )
+            criterion = crit_constructor(model, q, neg_sample_size)
+            optimizer.zero_grad()
+            with torch.no_grad():
+                loss = criterion.crit(y, None)
+                batch_losses.append(loss.item())
+                # print(loss)
+            # Calculate and assign gradients
+            criterion.calculate_crit_grad(y, idx)
+
+            # Take gradient step
+            optimizer.step()
+
+            # running_loss += loss.item()
+            batch_metrics.append(evaluation_metric(model))
+        if stopping_condition(
+            torch.nn.utils.parameters_to_vector(model.parameters()), old_params
+        ):
+            print("Training converged")
+            break
+    return torch.tensor(batch_losses), torch.tensor(batch_metrics)
--- src/training/training_utils.py
+++ src/training/training_utils.py
@@ -10,32 +10,51 @@ class PrecisionErrorMetric:
         self.true_precison = true_precision
 
     def metric(self, model):
-        return torch.mean((torch.exp(model.log_precision) - self.true_precison) ** 2)
+        return torch.mean(
+            (torch.exp(model.log_precision) - self.true_precison) ** 2
+        ).item()
+
 
 class Mse:
-    def __init__(self, true_model):
-        self.mu = true_model.mu
-        self.cov = true_model
+    def __init__(self, true_mu):
+        self.mu = true_mu
 
     def metric(self, model):
-        return torch.mean((self.mu - model.mu)**2)
+        return torch.mean((self.mu - model.mu) ** 2).item()
 
-class KlDiv:
-    def __init__(self, true_model):
-        self.true_model = true_model
+
+class MvnKlDiv:
+    """KL divergence between two MVN distributions"""
+
+    def __init__(self, true_mu, true_cov):
+        self.dim = true_cov.size(0)
+        self.true_mu = true_mu.reshape((self.dim, 1))
+        self.true_cov = true_cov
 
     def metric(self, model):
-        return torch.mean((torch.exp(model.log_precision) - self.true_precison) ** 2)
+        mu, cov = model.mu.reshape((self.dim, 1)), model.cov()
+        term_1 = torch.logdet(cov) - torch.logdet(self.true_cov)
+        term_2 = torch.trace(torch.linalg.solve(cov, self.true_cov))
+
+        mu_diff = mu - self.true_mu
+        cov_inv = torch.linalg.inv(cov)
+        term_3 = mu_diff.T @ cov_inv @ mu_diff
+        kl_div = (term_1 + term_2 + term_3 - self.dim) / 2
+        return kl_div.item()
+
 
 # Stopping conditions
-def no_stopping(new_params, old_params):
+def no_stopping(_new_params, _old_params):
     return False
 
 
 # From https://github.com/ciwanceylan/CNCE-matlab/blob/master/matlab/synthetic_data/bin/estimation/cnce.m
 def no_change_stopping_condition(new_params, old_params, tol=1e-4):
 
-    step_condition = torch.sqrt(torch.sum((new_params - old_params)**2)) < torch.sqrt(torch.sum(new_params**2)) * tol
+    step_condition = (
+        torch.sqrt(torch.sum((new_params - old_params) ** 2))
+        < torch.sqrt(torch.sum(new_params ** 2)) * tol
+    )
     if step_condition:
         return True
     else:
@@ -51,7 +70,9 @@ def add_to_npy_file(file_name, addition):
 
     if os.path.exists(Path(file_name)):
         prev = np.load(file_name)
-        assert prev.shape[0] == addition.shape[0], "Addition does not match previously saved array"
+        assert (
+            prev.shape[0] == addition.shape[0]
+        ), "Addition does not match previously saved array"
         np.save(file_name, np.column_stack((prev, addition)))
     else:
         # TODO: check that directory exists
