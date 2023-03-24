d4a1f8d6fa4e309fd11c565b430c10659807bae1
Amanda Olmin <amaol67@liu.se>
2023-03-24 17:08:58 +0100

    Adding P-CNCE + MH

--- src/nce/cd_mh_cnce.py
+++ src/nce/cd_mh_cnce.py
@@ -24,6 +24,7 @@ class CdMHCnceCrit(PartFnEstimator):
 
         self.mcmc_steps = mcmc_steps
         self.save_acc_prob = save_acc_prob
+        self.name = "cd_mh"
 
     def inner_crit(self, y: Tensor, y_samples: Tensor) -> Tensor:
         pass
@@ -31,14 +32,19 @@ class CdMHCnceCrit(PartFnEstimator):
     def calculate_crit_grad(self, y: Tensor, _idx: Optional[Tensor]):
         # We will have N*J pairs
         y = torch.repeat_interleave(y, self._num_neg, dim=0)
-        y_samples = self.sample_noise((y.size(0), 1), y)
+        y_samples = self.sample_noise(1, y) #self.sample_noise((y.size(0), 1), y)
 
         return self.calculate_inner_crit_grad(y, y_samples)
 
-    def calculate_inner_crit_grad(self, y: Tensor, y_samples: Tensor):
+    def calculate_inner_crit_grad(self, y: Tensor, y_samples: Tensor, y_base=None):
 
         # Gradient of mean is same as mean of gradient
-        grads_log_prob_y = self._unnorm_distr.grad_log_prob(y)
+        if y_base is None:
+            # Gradient of mean is same as mean of gradient
+            grads_log_prob_y = self._unnorm_distr.grad_log_prob(y)
+        else:
+            grads_log_prob_y = self._unnorm_distr.grad_log_prob(y_base)
+
         grads = [-grad_log_prob_y for grad_log_prob_y in grads_log_prob_y]
 
         y_0 = y.clone()
@@ -59,8 +65,8 @@ class CdMHCnceCrit(PartFnEstimator):
             if self.save_acc_prob:
                 # Ref. CNCE acc. prob.
                 acc_prob_cnce = 1 - 1 / (1 + torch.exp(-log_w_y))
-                add_to_npy_file("res/" + "cd_mh_num_neg_" + str(self._num_neg) + "_cd_mh_acc_prob.npy", w_y.numpy())
-                add_to_npy_file("res/" + "cd_mh_num_neg_" + str(self._num_neg) + "_cd_cnce_acc_prob.npy", acc_prob_cnce.numpy())
+                add_to_npy_file("res/" + self.name + "_num_neg_" + str(self._num_neg) + "_cd_mh_acc_prob.npy", w_y.numpy())
+                add_to_npy_file("res/" + self.name + "_num_neg_" + str(self._num_neg) + "_cd_cnce_acc_prob.npy", acc_prob_cnce.numpy())
 
             # Calculate gradients of log prob
             grads_log_prob = self._unnorm_distr.grad_log_prob(ys, w)
@@ -81,7 +87,7 @@ class CdMHCnceCrit(PartFnEstimator):
                 assert y_0.shape == y.shape
 
                 # Sample neg. samples
-                y_samples = self.sample_noise((y_0.size(0), 1), y_0)
+                y_samples = self.sample_noise(1, y)  # self.sample_noise((y_0.size(0), 1), y_0)
 
         self._unnorm_distr.set_gradients(grads)
 
@@ -89,10 +95,10 @@ class CdMHCnceCrit(PartFnEstimator):
         """Compute Ẑ"""
         pass
 
-    def sample_noise(self, num_samples: tuple, y: Tensor):
-        return self._noise_distr.sample(
-            torch.Size(num_samples), y.reshape(y.size(0), 1, -1)
-        )
+    #def sample_noise(self, num_samples: tuple, y: Tensor):
+    #    return self._noise_distr.sample(
+     #       torch.Size(num_samples), y.reshape(y.size(0), 1, -1)
+     #   )
 
     def _unnorm_w(self, y, y_samples) -> Tensor:
         return torch.exp(self._log_unnorm_w(y, y_samples))
---
+++ src/nce/per_mh_cnce_batch.py
@@ -0,0 +1,82 @@
+"""Persistent Conditional Noise Contrastive Estimation (CNCE)
+
+Inspired by Contrastive Divergence (CD) a persistent y is saved from the previous iteration.
+
+The method works like CNCE (`CondNceCrit`) but it has a persistent sample yp
+that is used to condition on when sampling noisy samples.
+"""
+from typing import Optional
+import torch
+from torch import Tensor
+from torch.distributions import Categorical
+
+from src.nce.cd_mh_cnce import CdMHCnceCrit
+from src.part_fn_utils import concat_samples, log_cond_unnorm_weights
+
+
+class PersistentMHCnceCritBatch(CdMHCnceCrit):
+    """Persistent cond. NCE crit"""
+
+    def __init__(self, unnorm_distr, noise_distr, num_neg_samples: int, save_acc_prob=False):
+        mcmc_steps = 1  #TODO: If we want to take several MCMC-steps, persistent y should  be updated at end of gradient calculation?
+        super().__init__(unnorm_distr, noise_distr, num_neg_samples, mcmc_steps, save_acc_prob)
+        self._persistent_y = dict()
+        self.name = "pers_mh_cnce"
+
+    def calculate_crit_grad(self, y: Tensor, idx: Optional[Tensor]) -> Tensor:
+        assert (
+            idx is not None
+        ), "PersistentCondNceCrit requires an idx tensor that is not None"
+
+        # TODO: The best would be to restructure y as a N*J x D matrix throughout this whole process (as in cd_cnce)
+        #   This is in line with having pairs (y_0, y_1). Maybe, we could then make a dict with keys idx_0, ..., idx_J
+        #   or similar
+
+        # TODO: this is a bit of an override, and should be made nice if we keep it
+        idx = torch.arange(start=0, end=y.shape[0])
+
+        y = y.unsqueeze(dim=1).repeat(1, self._num_neg, 1)
+
+        assert torch.allclose(y[0, 0, :], y[0, 1, :])
+
+        y_p = self.persistent_y(y, idx).reshape(-1, y.shape[-1])
+        y_samples = self.sample_noise(1, y_p)
+
+        # NB We recompute w_tilde in inner_crit to comply with the API.
+        w_y = torch.exp(-self._log_unnorm_w_ratio(y_p, y_samples).detach())
+        w_y[w_y >= 1.0] = 1.0
+        w = torch.cat((1 - w_y, w_y), dim=1).unsqueeze(dim=1)
+
+        self._update_persistent_y(w, y_p, y_samples, idx)
+
+        return self.calculate_inner_crit_grad(y_p, y_samples, y.reshape(-1, y.shape[-1]))
+
+    def persistent_y(self, actual_y: Tensor, idx: Tensor):
+        """Get persistent y
+
+        Access the last selected y or return the y sampled from p_d
+        if no last selected y exists
+        """
+
+        per_y = torch.empty(actual_y.size())
+        for n, per_n in enumerate(idx):
+            per_n = per_n.item()
+            per_y[n, :, :] = (
+                self._persistent_y[per_n]
+                if self._persistent_y.get(per_n) is not None
+                else actual_y[n, :, :]
+            )
+        return per_y
+
+    def _update_persistent_y(self, w, y, y_samples, idx):
+        """Sample new persistent y"""
+        ys = concat_samples(y, y_samples)
+        assert ys.shape == (y.shape[0], 2, y.shape[1])
+        assert len(idx) == (y.shape[0] / self._num_neg)
+
+        with torch.no_grad():
+            for n, i in zip(range(len(idx)), range(0, y.shape[0], self._num_neg)):
+                self._persistent_y[idx[n].item()] = torch.stack([ys[i+j,
+                                                                 Categorical(probs=w[i+j, 0, :]).sample(), :]
+                                                                 for j in range(self._num_neg)], dim=0)
+
