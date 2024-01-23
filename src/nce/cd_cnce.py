Correcting CD+CNCE gradient

src/nce/cd_cnce.py
@@ -44,7 +44,7 @@ class CdCnceCrit(PartFnEstimator):
         else:
             grads_log_prob_y = self._unnorm_distr.grad_log_prob(y_base)
 
-        grads = [-grad_log_prob_y / y.shape[0]  for grad_log_prob_y in grads_log_prob_y]
+        grads = [-2 * grad_log_prob_y / y.shape[0]  for grad_log_prob_y in grads_log_prob_y]
 
         y_0 = y.clone()
         for t in range(self.mcmc_steps):
@@ -55,7 +55,6 @@ class CdCnceCrit(PartFnEstimator):
 
             # Calculate and normalise weight ratios
             log_w_y = self._log_unnorm_w_ratio(y_0, y_samples).detach()
-            print(log_w_y.shape)
             w_y = 1 / (1 + torch.exp(-log_w_y))
             w = torch.cat((w_y, 1 - w_y), dim=1)
 